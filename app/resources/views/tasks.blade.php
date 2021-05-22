d2d6f3211711d64ecb69d53fa92e4f5a7604cf0d
igarashi <shogo.igarashi@asia-quest.jp>
2021-05-22 17:07:47 +0900

    削除ボタンの表示

--- app/resources/views/tasks.blade.php
+++ app/resources/views/tasks.blade.php
@@ -3,6 +3,38 @@
 @section('content')
     <!-- タスクフォームの作成 -->
 
+    <!-- Bootstrapの定形コード… -->
+
+    <div class="panel-body">
+        <!-- バリデーションエラーの表示 -->
+    @include('common.errors')
+
+    <!-- 新タスクフォーム -->
+        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+
+        <!-- タスク名 -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">タスク</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- タスク追加ボタン -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> タスク追加
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- TODO: 現在のタスク -->
+
     <!-- 現在のタスク -->
     @if (count($tasks) > 0)
         <div class="panel panel-default">
@@ -28,8 +60,16 @@
                                 <div>{{ $task->name }}</div>
                             </td>
 
+                            <!-- 削除ボタン -->
                             <td>
-                                <!-- TODO: 削除ボタン -->
+                                <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fa fa-trash"></i> 削除
+                                    </button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
