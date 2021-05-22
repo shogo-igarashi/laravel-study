ed144c8b1760da3c96a7ea4a8694cf27b2a04f8e
igarashi <shogo.igarashi@asia-quest.jp>
2021-05-22 16:08:34 +0900

    bootsrap の適用

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <title>Laravel Quickstart - Basic</title>
 
     <!-- CSSとJavaScript -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -14,5 +15,7 @@
 </div>
 
 @yield('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
